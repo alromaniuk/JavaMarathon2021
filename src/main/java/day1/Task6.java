4bcce2a598419a222eeb513b756d05a1227c2608
Alex <alex.romanyuk2017@gmail.com>
2021-01-20 13:08:38 +0200

    Day1

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 2;
+        int i = 0;
+        while (i <= 9) {
+            System.out.print(i + " x " + k);
+            System.out.println(" = " + i * k);
+            i++;
+        }
     }
 }
